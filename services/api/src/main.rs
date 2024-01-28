74f71e4fcbf275eae350f34080a08f7447ff2f2d
Lost <pandagaminggr@gmail.com>
2024-01-29 00:33:41 +0200

    chore: add .env file; remove .env from gitignore; refactor

--- services/api/src/main.rs
+++ services/api/src/main.rs
@@ -5,7 +5,7 @@ use warp::Filter;
 async fn main() {
     // Match any request and return hello world!
     let routes = warp::any().map(|| "Hello, World!");
-    let client = prisma_client::build();
+    let _client = prisma_client::build();
 
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
